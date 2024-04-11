d6aea9f2ac44fd603fd4804f6d22bc786963d002
AlinaKlochai <mrs.ali@tuta.io>
2024-04-11 05:44:50 +0200

    07

---
+++ homeworks/homework_07/pages/garten/index.js
@@ -0,0 +1,17 @@
+ // Получаем все кнопки "Add to corb"
+ var buttons = document.querySelectorAll('.main_button');
+        
+ // Добавляем обработчик события для каждой кнопки
+ buttons.forEach(function(button) {
+     button.addEventListener('click', function() {
+         // Получаем id товара
+         var itemId = this.closest('.products').id;
+         
+         // Создаем новый элемент для отображения товара в корзине
+         var newItem = document.createElement('div');
+         newItem.textContent = itemId;
+         
+         // Добавляем новый элемент в блок с корзиной
+         document.getElementById('cart_items').appendChild(newItem);
+     });
+ });
