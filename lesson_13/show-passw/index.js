67ff8d2ee474b725655269ff046275ccfa3bb1b9
AlinaKlochai <mrs.ali@tuta.io>
2024-04-16 13:01:44 +0200

    lesson13

--- lesson_13/show-passw/index.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', ()=>{
-    const pass = document.getElementById('password');
-    pass.setAttribute('type', 'text');
-});
-
-
-
