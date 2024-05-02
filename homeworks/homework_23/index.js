35f297ed40827f3f32283e285b275a1934d82ef6
AlinaKlochai <mrs.ali@tuta.io>
2024-05-02 08:51:34 +0200

    23

--- homeworks/homework_23/index.js
+++ homeworks/homework_23/index.js
@@ -8,7 +8,7 @@ function getAllUnis() {
     fetch(url)
         .then(response => {
             if (!response.ok) {
-                throw new Error('Failed to fetch universities data');
+                throw new Error('Failed to load university data');
             }
             return response.json();
         })
@@ -17,16 +17,16 @@ function getAllUnis() {
             data.forEach(university => {
                 const universityItem = document.createElement('li'); // Создаем элемент <li> для каждого университета
 
-                // Если у университета есть веб-сайт, создаем ссылку для перехода
+                //если у университета есть веб-сайт, создаем ссылку для перехода
                 if (university.web_pages && university.web_pages.length > 0) {
                     const websiteLink = document.createElement('a');
-                    websiteLink.textContent = university.name;
-                    websiteLink.href = university.web_pages[0]; // Берем первый веб-сайт из списка (если у университета их несколько)
-                    websiteLink.target = '_blank'; // Открываем ссылку в новой вкладке
-                    websiteLink.rel = 'noopener noreferrer'; // Добавляем рекомендуемые атрибуты безопасности
-                    universityItem.appendChild(websiteLink); // Добавляем ссылку в элемент <li>
+                    websiteLink.textContent = university.name;                   // Устанавливаем текст ссылки равным имени университета
+                    websiteLink.href = university.web_pages[0];                 // Устанавливаем адрес ссылки равным первому веб-сайту университета
+                    websiteLink.target = '_blank';                             // Открываем ссылку в новой вкладке
+                    websiteLink.rel = 'noopener noreferrer';                  // Добавляем рекомендуемые атрибуты безопасности
+                    universityItem.appendChild(websiteLink);                 // Добавляем ссылку в элемент <li>
                 } else {
-                    universityItem.textContent = university.name; // Если веб-сайта нет, добавляем просто текст
+                    universityItem.textContent = university.name;          // Если веб-сайта нет, добавляем просто текст
                 }
 
                 universitiesList.appendChild(universityItem); // Добавляем элемент <li> в элемент <ul>
